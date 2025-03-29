9b6b0b2026826be64ae6a7ef7fdb941c28294dea
glorl <glorl@gmx.de>
2025-03-29 21:46:44 +0100

    Missa solemnis

---
+++ +source/lily2dict.py
@@ -0,0 +1,8 @@
+import re
+
+text = open('../+lilypond/Mozart_MissaSolemnisC/MissaSolemnisC_zwei.ly')
+piecenames = re.findall('(.*)[ ]=', text.read())
+for piecename in piecenames:
+    print(piecename.strip())
+pieces = re.findall('.+[ ]=[ ](\w*)', text.read())
+print(pieces)
